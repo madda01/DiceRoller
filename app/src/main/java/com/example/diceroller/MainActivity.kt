df11f1f0e8419e86bc15ecd68c5af7ffcad33866
Madhumini Gunaratne <79505612+madda01@users.noreply.github.com>
2021-08-06 20:50:58 +0530

    Update MainActivity.kt

--- app/src/main/java/com/example/diceroller/MainActivity.kt
+++ app/src/main/java/com/example/diceroller/MainActivity.kt
@@ -26,7 +26,7 @@ class MainActivity : AppCompatActivity() {
             3 -> R.drawable.dice_3
             4 -> R.drawable.dice_4
             5 -> R.drawable.dice_5
-            else -> R.drawable.dice_6
+            else -> R.drawable.dice_6 //rolling
         }
         diceImage.setImageResource(drawresource)
 
